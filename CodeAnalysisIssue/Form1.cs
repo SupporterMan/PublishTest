00cd7994545dc4f77eb2f82c26a6e20479689fa7
SupporterMan <v-weiwca@microsoft.com>
2018-02-22 14:22:59 +0800

    //Test
    Add comment //Test

--- CodeAnalysisIssue/Form1.cs
+++ CodeAnalysisIssue/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+//﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +13,7 @@ namespace CodeAnalysisIssue
     public partial class Form1 : Form
     {
         public Form1()
-        {
+        { //Test
             InitializeComponent();
         }
 
