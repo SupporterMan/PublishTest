bee17bc141e1a21e740a74ed1cf643869111baae
SupporterMan <v-weiwca@microsoft.com>
2018-02-22 14:32:27 +0800

    asdad
    asdad

--- CodeAnalysisIssue/Form1.cs
+++ CodeAnalysisIssue/Form1.cs
@@ -1,4 +1,4 @@
-//﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,7 +18,7 @@ namespace CodeAnalysisIssue
         }
 
         private void doNothing()
-        {
+        { asdad
             int a;
         }
     }
