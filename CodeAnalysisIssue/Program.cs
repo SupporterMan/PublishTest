c303569bc1c7fd6b9e9cc20c49bbdbcf64239c78
Weiwei Cai <v-weiwca@hotmail.com>
2018-06-23 09:28:56 +0800

    Revert "aaa"
    This reverts commit d91a1ade3e6d4ceac7086faa15b6da2bfe6e7073.

--- CodeAnalysisIssue/Program.cs
+++ CodeAnalysisIssue/Program.cs
@@ -17,7 +17,6 @@ namespace CodeAnalysisIssue
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //qweqweqwe
         }
     }
 }
